4da3c36878a3140f696a5bf86014d1fdf35baa15
bradleyalden <alden.bradley@nist.gov>
2026-07-22 11:51:19 -0600

    easier to switch methods

--- execute_fragmentation.py
+++ execute_fragmentation.py
@@ -1,21 +1,15 @@
 import operator
 from rdkit import Chem
 from fragmenter import fragmenter
-# from fragmenter_utils import draw_mol_with_highlights_and_legend
+from fragmenter_utils import draw_mol_with_highlights_and_legend
 # from pprint import pprint
 # from rdkit.Chem import rdmolops
 import csv
 import SMARTS
-import SMARTS_MARGAN
-import SMARTS_MARGAN_GEM
-import SMARTS_TDE
 import sort_list
 
 # choose which method to use
-# SMARTS_LIST = SMARTS.UNIFAC.copy()
-# SMARTS_LIST = SMARTS_MARGAN.MARGAN.copy()
-SMARTS_LIST = SMARTS_MARGAN_GEM.MARGAN.copy()
-# SMARTS_LIST = SMARTS_TDE.UNIFAC.copy()
+SMARTS_LIST = SMARTS.UNIFAC_TDE.copy()
 
 # get the fragmentation scheme in the format necessary
 fragmentation_scheme = {i+1: j[1] for i, j in enumerate(SMARTS_LIST)}
@@ -65,7 +59,7 @@ def create_fragmentation_scheme_order():
     return fragmentation_scheme_order
 fragmentation_scheme_order_margan = create_fragmentation_scheme_order()
 
-def function_to_choose_fragmentation(fragmentations):
+def function_to_choose_fragmentation_margan(fragmentations):
     """
         Selects a fragmentation from a list of fragmentation for Marrero-Gani method
         checks to ensure fragmentations have used non-split groups where applicable
@@ -122,15 +116,66 @@ def function_to_choose_fragmentation(fragmentations):
             
     return best_fragmentation
 
-    # fragmentations_descriptors = {}
-    # i = 0
-    # for fragmentation in fragmentations:
-    #     fragmentations_descriptors[i] = [len(fragmentation)]
-    #     i += 1
+def function_to_choose_fragmentation_unifac(fragmentations):
+    """
+        Selects a fragmentation from a list of fragmentation for UNIFAC method
+        selects the fragmentation with the heaviest single group
+    """
+    best_fragmentation = None
+    best_score = None
     
-    # sorted_fragmentations_dict = sorted(fragmentations_descriptors.items(), key=operator.itemgetter(1))
+    # Cache to store SMARTS weights so we don't recalculate them across loops
+    weight_cache = {}
+    group_names = {
+        s: name 
+        for name, smarts in SMARTS_LIST 
+        for s in (smarts if isinstance(smarts, list) else [smarts])
+    }
+    for frag in fragmentations:
+        group_weights = []
+        total_groups = 0
+        urea_count = 0
+        ac_r_count = 0
+        
+        for smarts, matches in frag.items():
+            num_matches = len(matches)
+            total_groups += num_matches
+            
+            if smarts not in weight_cache:
+                mol = Chem.MolFromSmarts(smarts)
+                weight = 0.0
+                if mol:
+                    for atom in mol.GetAtoms():
+                        weight += atom.GetMass()
+                        query = atom.DescribeQuery()
+                        if "AtomHCount" in query:
+                            num_h = int(query.split('AtomHCount')[1][1])
+                            weight += num_h * 1.008
+                            
+                weight_cache[smarts] = weight
+            
+            group_weights.extend([weight_cache[smarts]] * num_matches)
+            
+        group_weights.sort(reverse=True)
 
-    # return fragmentations[sorted_fragmentations_dict[0][0]]
+        current_score = (group_weights, -total_groups)
+                
+        if best_score is None or current_score > best_score:
+            best_score = current_score
+            best_fragmentation = frag
+            
+    return best_fragmentation
+
+def function_to_choose_fragmentation_simple(fragmentations):
+    fragmentations_descriptors = {}
+    i = 0
+    for fragmentation in fragmentations:
+        fragmentations_descriptors[i] = [len(fragmentation)]
+        i += 1
+    
+    sorted_fragmentations_dict = sorted(fragmentations_descriptors.items(), key=operator.itemgetter(1))
+
+    return fragmentations[sorted_fragmentations_dict[0][0]]
 
 CUSTOM_PROPERTY_MATCH_FUNCTIONS = {
     "IsInRing": (
@@ -139,9 +184,9 @@ CUSTOM_PROPERTY_MATCH_FUNCTIONS = {
     ),
 }
 
-calculate_from = "csvs\\margan_groups_check.csv"
+calculate_from = "csvs\\smilesTest.csv"
 write_to = "group_lists\\fragmented_groups.txt"
-# sort_to = "group_lists\\fragmented_groups_sorted_corrected_margan.txt"
+sort_to = "group_lists\\fragmented_groups_sorted.txt"
 smiles = []
 molecule_ids = []
 start_offset = 0
@@ -163,17 +208,18 @@ frg = fragmenter(
     fragmentation_scheme,
     algorithm='complete',
     n_heavy_atoms_cuttoff=30,
-    function_to_choose_fragmentation=function_to_choose_fragmentation,
+    function_to_choose_fragmentation=function_to_choose_fragmentation_unifac,
     match_hydrogens=False,
     n_max_fragmentations_to_find=400,
     reject_fragmented_molecules=True,
     fragmentation_scheme_order=fragmentation_scheme_order_margan,
-    properties_to_match=["GetTotalNumHs", "GetFormalCharge", "IsInRing"],
+    properties_to_match=["GetTotalNumHs", "GetFormalCharge"],
     custom_property_match_functions=CUSTOM_PROPERTY_MATCH_FUNCTIONS
     )
 smiles_length = len(smiles)
 
-# ids removed because of errors/not solving: 19929, 13894, 30784, 22893
+# ids removed because of not solving: 19929, 13894; these are back in
+# ids removed because invalid SMILES: 30784, 22893
 
 for index, smi in enumerate(smiles[start_offset:]):
     # print(smi)
@@ -188,6 +234,11 @@ for index, smi in enumerate(smiles[start_offset:]):
             file.write("\n")
     if not success:
         n_noSolution += 1
+    
+    if (molecule_ids[index + start_offset] == "200000" or True) and success and False:
+        mol = Chem.MolFromSmiles(smi)
+        img = draw_mol_with_highlights_and_legend(mol, fragmentation_matches)
+        img.save(f'simple_example{index + 1}.png')
 
 
 print ("No Solution: ", n_noSolution)
